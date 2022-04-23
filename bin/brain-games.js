273032cb196c22b515f1f295b9b3f3984555ba24
andrey.sh <aash7133@gmail.com>
2022-04-23 11:36:41 +0700

    задачи по eslint выполнены

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { greetning } from '../src/cli.js'
+import greetning from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 greetning();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export function greetning() {
+function greetning() {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 }
+
+export default greetning;
